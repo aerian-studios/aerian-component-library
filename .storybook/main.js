c69d782880c80e9e9151c3c8d2404fd66187a6ef
Matt Hardman <matt.hardman@aerian.com>
2020-07-28 15:17:58 +0100

    update paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.story.(tsx|mdx)"],
+  stories: ["../src/components/**/*.story.(tsx|mdx)"],
   addons: ["@storybook/addon-docs"],
   webpackFinal: async (config) => {
     config.module.rules.push({
