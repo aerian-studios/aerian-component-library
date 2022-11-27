5c787dd6ded6e0075247ce8ac6773b17734313fb
Sunya Dickman <6469947+S-unya@users.noreply.github.com>
2022-11-27 15:47:03 +0000

    chore: checkbox update packages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 /* PLOP_INJECT_IMPORT */
+import { AerCheckbox } from "./AerCheckbox";
 import { AerAccordion } from "./AerAccordion";
 import { AerAlertDialog, AerAlertDialogFooter, AerAlertDialogTrigger } from "./AerAlertDialog";
 
 export {
   /* PLOP_INJECT_EXPORT */
+	AerCheckbox,
 	AerAccordion,
 	AerAlertDialog,
 	AerAlertDialogFooter,
