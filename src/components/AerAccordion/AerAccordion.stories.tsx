2bf9f8f9380d56708af884c8bce60a64455fc018
Sunya Dickman <6469947+S-unya@users.noreply.github.com>
2022-11-28 10:46:52 +0000

    style: checkbox update local themes for consistency

--- src/components/AerAccordion/AerAccordion.stories.tsx
+++ src/components/AerAccordion/AerAccordion.stories.tsx
@@ -60,29 +60,30 @@ export const LocalTheme: ComponentStory<any> = () => (
     <p>The AerAccordion component implements the following local theme</p>
     <pre>
       {`
-      root: border-radius: var(--s-cmp-border-radius, var(--s-border-radius-xs));
-      root: width: var(--s-cmp-width, unset);
-      root: box-shadow: var(--sh-cmp, var(--sh-box-xs));
-     
-      item-separator-color: var(--c-cmp-separator, var(--c-separator));// actually applied as a background to the root
-      item-separator-height: var(--c-cmp-separator-height, 1px);
-      item-focus-within-border-color: var(--c-cmp-focus, --c-focus);
-      item-focus-within-border-size: var(--s-cmp-focus, 2px);
-      
-      item-header:trigger: padding: var(--s-cmp-trigger-padding-top, 0) var(--s-cmp-trigger-padding-right, var(--s-2)) var(--s-cmp-trigger-padding-bottom, 0) var(--s-cmp-trigger-padding-left, var(--s-2));
-      item-header:trigger: height: var(--cmp-trigger-height, var(--t-heading-m));
-      item-header:trigger: font-size: var(--t-cmp-trigger, var(--t-heading-m));
-      item-header:trigger: color: var(--c-cmp-trigger);
-      item-header:trigger: box-shadow: var(--sh-cmp-trigger, var(--sh-box-xs));
-      item-header:trigger: background-color: var(--c-cmp-trigger-bg, var(--c-white));
-      item-header:trigger:hover: color: var(--c-cmp-trigger-hover);
-      item-header:trigger:hover: background-color: var(--c-cmp-trigger-bg-hover, var(--c-gray-100));
-      item-header:trigger-icon: color: var(--c-cmp-trigger-icon, inherit);
-      
-      item-content: color: var(--c-cmp-item);
-      item-content: font-size: var(--t-cmp-body, var(--t-body-m));
-      item-content: background-color: var(--c-cmp-body-bg, var(--c-gray-100));
-      item-content: padding: var(--s-cmp-body-padding-top, var(--s-1)) var(--s-cmp-body-padding-right, var(--s-2)) var(--s-cmp-body-padding-bottom, var(--s-1)) var(--s-cmp-body-padding-left, var(--s-2));
+      * root: border-radius: var(--s-cmp-accordion-border-radius, var(--s-border-radius-xs));
+      * root: width: var(--s-cmp-accordion-width, unset);
+      * root: box-shadow: var(--sh-cmp, var(--sh-box-xs));
+     *
+      * item-separator-color: var(--c-cmp-accordion-separator, var(--c-separator));// actually applied as a background to the root
+      * item-separator-height: var(--c-cmp-accordion-separator-height, 1px);
+      * item-focus-within-border-color: var(--c-cmp-accordion-focus, --c-focus);
+      * item-focus-within-border-size: var(--s-cmp-accordion-focus, 2px);
+      *
+      * item-header:trigger: padding: var(--s-cmp-accordion-trigger-padding-top, 0) var(--s-cmp-accordion-trigger-padding-right, var(--s-2)) var(--s-cmp-accordion-trigger-padding-bottom, 0) var(--s-cmp-accordion-trigger-padding-left, var(--s-2));
+      * item-header:trigger: height: var(--cmp-accordion-trigger-height, var(--t-heading-m));
+      * item-header:trigger: font-size: var(--t-cmp-accordion-trigger, var(--t-heading-m));
+      * item-header:trigger: color: var(--c-cmp-accordion-trigger);
+      * item-header:trigger: box-shadow: var(--sh-cmp-accordion-trigger, var(--sh-box-xs));
+      * item-header:trigger: background-color: var(--c-cmp-accordion-trigger-bg, var(--c-white));
+      * item-header:trigger:hover: color: var(--c-cmp-accordion-trigger-hover);
+      * item-header:trigger:hover: background-color: var(--c-cmp-accordion-trigger-bg-hover, var(--c-gray-100));
+      * item-header:trigger-icon: color: var(--c-cmp-accordion-trigger-icon, inherit);
+      * item-header:trigger-icon: width/height: var(--s-cmp-accordion-icon, var(--s-icon));
+      *
+      * item-content: color: var(--c-cmp-accordion-item);
+      * item-content: font-size: var(--t-cmp-accordion-body, var(--t-body-m));
+      * item-content: background-color: var(--c-cmp-accordion-body-bg, var(--c-gray-100));
+      * item-content: padding: var(--s-cmp-accordion-body-padding-top, var(--s-1)) var(--s-cmp-accordion-body-padding-right, var(--s-2)) var(--s-cmp-accordion-body-padding-bottom, var(--s-1)) var(--s-cmp-accordion-body-padding-left, var(--s-2));
       `}
     </pre>
   </>
