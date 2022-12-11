a52a44212e73536bedd78bcf4341a3fe7b39f9ab
Sunya Dickman <6469947+S-unya@users.noreply.github.com>
2022-12-11 19:19:57 +0000

    chore: precommit test

--- src/components/AerAlertDialog/AerAlertDialog.spec.tsx
+++ src/components/AerAlertDialog/AerAlertDialog.spec.tsx
@@ -5,118 +5,118 @@ import userEvent from "@testing-library/user-event";
 import { AerAlertDialog } from "./index";
 
 describe("AlertDialog", () => {
-  afterEach(() => {
-    cleanup();
-  });
+	afterEach(async () => {
+		cleanup();
+	});
 
-  it("should render correctly", async () => {
-    const { baseElement } = render(
-      <AerAlertDialog.Root
-        trigger={<button>Open the dialog</button>}
-        dialogTitle={<>This is important!</>}
-        footer={
-          <AerAlertDialog.Footer
-            cancel={<button>Cancel</button>}
-            action={<button>Yes, do it!</button>}
-          />
-        }
-      >
-        <p>Are you sure about all of that stuff?</p>
-      </AerAlertDialog.Root>
-    );
+	it("should render correctly", async () => {
+		const { baseElement } = render(
+			<AerAlertDialog.Root
+				trigger={<button>Open the dialog</button>}
+				dialogTitle={<>This is important!</>}
+				footer={
+					<AerAlertDialog.Footer
+						cancel={<button>Cancel</button>}
+						action={<button>Yes, do it!</button>}
+					/>
+				}
+			>
+				<p>Are you sure about all of that stuff?</p>
+			</AerAlertDialog.Root>,
+		);
 
-    await userEvent.click(
-      screen.getByRole("button", { name: "Open the dialog" })
-    );
+		await userEvent.click(
+			screen.getByRole("button", { name: "Open the dialog" }),
+		);
 
-    expect(screen.getByRole("alertdialog", { name: "This is important!" }));
-    expect(screen.getByText("Are you sure about all of that stuff?"));
+		expect(screen.getByRole("alertdialog", { name: "This is important!" }));
+		expect(screen.getByText("Are you sure about all of that stuff?"));
 
-    expect(baseElement).toMatchSnapshot();
-  });
+		expect(baseElement).toMatchSnapshot();
+	});
 
-  it("should render an accessible `alertdialog` despite not showing the title visually", async () => {
-    const { baseElement } = render(
-      <AerAlertDialog.Root
-        trigger={<button>Open the dialog</button>}
-        dialogTitle={{ text: <>This is important!</>, hide: true }}
-        footer={
-          <AerAlertDialog.Footer
-            cancel={<button>Cancel</button>}
-            action={<button>Yes, do it!</button>}
-          />
-        }
-      >
-        <p>Are you sure about all of that stuff?</p>
-      </AerAlertDialog.Root>
-    );
+	it("should render an accessible `alertdialog` despite not showing the title visually", async () => {
+		const { baseElement } = render(
+			<AerAlertDialog.Root
+				trigger={<button>Open the dialog</button>}
+				dialogTitle={{ text: <>This is important!</>, hide: true }}
+				footer={
+					<AerAlertDialog.Footer
+						cancel={<button>Cancel</button>}
+						action={<button>Yes, do it!</button>}
+					/>
+				}
+			>
+				<p>Are you sure about all of that stuff?</p>
+			</AerAlertDialog.Root>,
+		);
 
-    await userEvent.click(
-      screen.getByRole("button", { name: "Open the dialog" })
-    );
+		await userEvent.click(
+			screen.getByRole("button", { name: "Open the dialog" }),
+		);
 
-    expect(screen.getByRole("alertdialog", { name: "This is important!" }));
-    expect(baseElement).toMatchSnapshot();
-  });
+		expect(screen.getByRole("alertdialog", { name: "This is important!" }));
+		expect(baseElement).toMatchSnapshot();
+	});
 
-  it("should call the cancel callback and hide the dialog", async () => {
-    const cancelCb = vi.fn();
+	it("should call the cancel callback and hide the dialog", async () => {
+		const cancelCb = vi.fn();
 
-    render(
-      <AerAlertDialog.Root
-        trigger={<button>Open the dialog</button>}
-        dialogTitle={<>This is important!</>}
-        footer={
-          <AerAlertDialog.Footer
-            cancel={<button onClick={cancelCb}>Cancel</button>}
-            action={<button>Yes, do it!</button>}
-          />
-        }
-      >
-        <p>Are you sure about all of that stuff?</p>
-      </AerAlertDialog.Root>
-    );
+		render(
+			<AerAlertDialog.Root
+				trigger={<button>Open the dialog</button>}
+				dialogTitle={<>This is important!</>}
+				footer={
+					<AerAlertDialog.Footer
+						cancel={<button onClick={cancelCb}>Cancel</button>}
+						action={<button>Yes, do it!</button>}
+					/>
+				}
+			>
+				<p>Are you sure about all of that stuff?</p>
+			</AerAlertDialog.Root>,
+		);
 
-    await userEvent.click(
-      screen.getByRole("button", { name: "Open the dialog" })
-    );
+		await userEvent.click(
+			screen.getByRole("button", { name: "Open the dialog" }),
+		);
 
-    await userEvent.click(screen.getByRole("button", { name: /cancel/i }));
+		await userEvent.click(screen.getByRole("button", { name: /cancel/i }));
 
-    expect(cancelCb).toHaveBeenCalledOnce();
+		expect(cancelCb).toHaveBeenCalledOnce();
 
-    expect(
-      screen.queryByRole("alertdialog", { name: "This is important!" })
-    ).toBeNull();
-  });
+		expect(
+			screen.queryByRole("alertdialog", { name: "This is important!" }),
+		).toBeNull();
+	});
 
-  it("should call the action callback and hide the dialog", async () => {
-    const actionCb = vi.fn();
+	it("should call the action callback and hide the dialog", async () => {
+		const actionCb = vi.fn();
 
-    render(
-      <AerAlertDialog.Root
-        trigger={<button>Open the dialog</button>}
-        dialogTitle={"This is important!"}
-        footer={
-          <AerAlertDialog.Footer
-            cancel={<button>Cancel</button>}
-            action={<button onClick={actionCb}>Yes, do it!</button>}
-          />
-        }
-      >
-        <p>Are you sure about all of that stuff?</p>
-      </AerAlertDialog.Root>
-    );
+		render(
+			<AerAlertDialog.Root
+				trigger={<button>Open the dialog</button>}
+				dialogTitle={"This is important!"}
+				footer={
+					<AerAlertDialog.Footer
+						cancel={<button>Cancel</button>}
+						action={<button onClick={actionCb}>Yes, do it!</button>}
+					/>
+				}
+			>
+				<p>Are you sure about all of that stuff?</p>
+			</AerAlertDialog.Root>,
+		);
 
-    await userEvent.click(
-      screen.getByRole("button", { name: "Open the dialog" })
-    );
+		await userEvent.click(
+			screen.getByRole("button", { name: "Open the dialog" }),
+		);
 
-    await userEvent.click(screen.getByRole("button", { name: /yes, do it/i }));
+		await userEvent.click(screen.getByRole("button", { name: /yes, do it/i }));
 
-    expect(actionCb).toHaveBeenCalledOnce();
-    expect(
-      screen.queryByRole("alertdialog", { name: "This is important!" })
-    ).toBeNull();
-  });
+		expect(actionCb).toHaveBeenCalledOnce();
+		expect(
+			screen.queryByRole("alertdialog", { name: "This is important!" }),
+		).toBeNull();
+	});
 });
