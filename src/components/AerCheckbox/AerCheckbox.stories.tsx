018205f77e3bb0c15d26888ead61f8d8f59504b4
Sunya Dickman <6469947+S-unya@users.noreply.github.com>
2022-12-11 16:59:05 +0000

    chore: precpmmit test

--- src/components/AerCheckbox/AerCheckbox.stories.tsx
+++ src/components/AerCheckbox/AerCheckbox.stories.tsx
@@ -3,33 +3,33 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { AerCheckbox } from "./index";
 
 const Meta: ComponentMeta<typeof AerCheckbox> = {
-  title: "Components/AerCheckbox",
-  component: AerCheckbox,
+	title: "Components/AerCheckbox",
+	component: AerCheckbox,
 };
 
 export default Meta;
 
 const Template: ComponentStory<typeof AerCheckbox> = (args) => (
-  <AerCheckbox {...args} />
+	<AerCheckbox {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
+	label: "Accept the terms and conditions",
+	value: "t&c",
 };
 
 export const LocalTheme: ComponentStory<any> = () => (
-  <>
-    <p>The AerCheckbox component implements the following local theme</p>
-    <p>
-      <em>
-        NOTE: currently the AerCheckbox has some default behaviour for
-        invalidity that might need work in the future
-      </em>
-    </p>
-    <pre>
-      {`
+	<>
+		<p>The AerCheckbox component implements the following local theme</p>
+		<p>
+			<em>
+				NOTE: currently the AerCheckbox has some default behaviour for
+				invalidity that might need work in the future
+			</em>
+		</p>
+		<pre>
+			{`
       input: gap: var(--s-cmp-checkbox, var(--s-1));
       input:icon: color: var(--c-cmp-checkbox-icon, var(--c-body));
       input:icon: width/height: var(--s-cmp-checkbox-icon, var(--s-icon));
@@ -42,87 +42,88 @@ export const LocalTheme: ComponentStory<any> = () => (
       label: padding: var(--s-cmp-checkbox-padding, 0 0 0 var(--s-1));
       label: font-size: var(--t-cmp-checkbox, var(--c-body-m));
       `}
-    </pre>
-  </>
+		</pre>
+	</>
 );
 
 export const Checked = Template.bind({});
 Checked.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
-  defaultState: true,
+	label: "Accept the terms and conditions",
+	value: "t&c",
+	defaultState: true,
 };
 Checked.parameters = {
-  docs: {
-    description: {
-      story: "This is the dark variant of the component and it displays...",
-    },
-  },
+	docs: {
+		description: {
+			story: "This is the dark variant of the component and it displays...",
+		},
+	},
 };
 
 export const Indeterminate = Template.bind({});
 Indeterminate.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
-  defaultState: "indeterminate",
+	label: "Accept the terms and conditions",
+	value: "t&c",
+	defaultState: "indeterminate",
 };
 Indeterminate.parameters = {
-  docs: {
-    description: {
-      story:
-        "Indeterminate is only settable (and unsettable) via the `checked` prop; local state changes will not affect it",
-    },
-  },
+	docs: {
+		description: {
+			story:
+				"Indeterminate is only settable (and unsettable) via the `checked` prop; local state changes will not affect it",
+		},
+	},
 };
 
 export const Invalid = Template.bind({});
 Invalid.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
-  defaultState: true,
-  required: true,
+	label: "Accept the terms and conditions",
+	value: "t&c",
+	defaultState: true,
+	required: true,
 };
 Invalid.parameters = {
-  docs: {
-    description: {
-      story:
-        "Invalid elements will show an error message. To trigger the error on this required field, deselect the checkbox and tab or click outside",
-    },
-  },
+	docs: {
+		description: {
+			story:
+				"Invalid elements will show an error message. To trigger the error on this required field, deselect the checkbox and tab or click outside",
+		},
+	},
 };
 
 export const CustomCheckIcons = Template.bind({});
+
 CustomCheckIcons.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
-  defaultState: false,
-  checkBox: <span style={{ border: "2px solid green" }}></span>,
-  checkedIcon: (
-    <span style={{ display: "flex", justifyContent: "center" }}>💝</span>
-  ),
+	label: "Accept the terms and conditions",
+	value: "t&c",
+	defaultState: false,
+	checkBox: <span style={{ border: "2px solid green" }}></span>,
+	checkedIcon: (
+		<span style={{ display: "flex", justifyContent: "center" }}>💝</span>
+	),
 };
 CustomCheckIcons.parameters = {
-  docs: {
-    description: {
-      story:
-        "CustomCheckIcons give you complete control over the look of the check box UI. The `checkBox` relates to the background; the `checkedIcon` relates to the icon to show when checked; and the `indeterminateIcon`, what to show when indeterminate",
-    },
-  },
+	docs: {
+		description: {
+			story:
+				"CustomCheckIcons give you complete control over the look of the check box UI. The `checkBox` relates to the background; the `checkedIcon` relates to the icon to show when checked; and the `indeterminateIcon`, what to show when indeterminate",
+		},
+	},
 };
 
 export const CustomInvalidMessage = Template.bind({});
 CustomInvalidMessage.args = {
-  label: "Accept the terms and conditions",
-  value: "t&c",
-  defaultState: false,
-  required: true,
-  errorMessage: "Beep Boop! Please fill in this field.",
+	label: "Accept the terms and conditions",
+	value: "t&c",
+	defaultState: false,
+	required: true,
+	errorMessage: "Beep Boop! Please fill in this field.",
 };
 CustomInvalidMessage.parameters = {
-  docs: {
-    description: {
-      story:
-        "It is possible to override the validity by providing a custom `errorMessage` prop. NOTE: The component will remain invalid until the `errorMessage` prop is `falsy`",
-    },
-  },
+	docs: {
+		description: {
+			story:
+				"It is possible to override the validity by providing a custom `errorMessage` prop. NOTE: The component will remain invalid until the `errorMessage` prop is `falsy`",
+		},
+	},
 };
